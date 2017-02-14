8bf5413c17d5ae46625740557dd5247190afa251
Mary Wahl <mawah@microsoft.com>
2017-02-14 08:53:55 -0500

    Retraining final layer of AlexNet; slight change to image set preparation

---
+++ cntk/retrain.py
@@ -0,0 +1,113 @@
+# Copyright (c) Microsoft. All rights reserved.
+#
+# Modified by Mary Wahl from the Object Detection with Fast-R-CNN CNTK example script at:
+# https://github.com/Microsoft/CNTK/blob/master/Examples/Image/Detection/FastRCNN/A2_RunCntk_py3.py
+#
+# Licensed under the MIT license. See LICENSE.md file in the project root
+# for full license information.
+# ==============================================================================
+
+''' edit as necessary '''
+output_model_folder = 'D:\\cntk\\output_models'
+map_file = 'D:\\combined\\train_subsample\\map.txt'
+model_file = "D:\\cntk\\AlexNet.model"
+
+from __future__ import print_function
+from cntk import Trainer, UnitType, load_model
+from cntk.layers import Placeholder, Constant, Dense
+from cntk.graph import find_by_name
+from cntk.io import MinibatchSource, ImageDeserializer, StreamDef, StreamDefs
+from cntk.learner import momentum_sgd, learning_rate_schedule, momentum_as_time_constant_schedule
+from cntk.ops import input_variable, parameter, cross_entropy_with_softmax, classification_error, combine
+from cntk.ops.functions import CloneMethod
+from cntk.utils import log_number_of_parameters, ProgressPrinter
+import numpy as np
+import os, sys
+
+num_channels = 3
+image_height = 224
+image_width = 224
+num_classes = 6
+epoch_size = 50000
+mb_size = 16
+max_epochs = 50
+
+def create_reader(map_file):
+    transforms = [ImageDeserializer.crop(crop_type='randomarea', area_ratio=[0.85,1.0],
+                                         jitter_type='uniratio'),
+                  ImageDeserializer.scale(width=image_width,
+                                          height=image_height,
+                                          channels=num_channels,
+                                          interpolations='linear'),
+                  ImageDeserializer.color(brightness_radius=0.2,
+                                          contrast_radius=0.2,
+                                          saturation_radius=0.2)]
+    return(MinibatchSource(ImageDeserializer(map_file, StreamDefs(
+        features = StreamDef(field='image', transforms=transforms, is_sparse=False),
+        labels   = StreamDef(field='label', shape=num_classes, is_sparse=False)))))
+
+def frcn_predictor(features, n_classes):
+    loaded_model = load_model(model_file)
+    feature_node = find_by_name(loaded_model, 'features')
+    last_node    = find_by_name(loaded_model, 'h2_d')
+    all_layers = combine([last_node.owner]).clone(CloneMethod.freeze, {feature_node: Placeholder()})
+
+    feat_norm = features - Constant(114)
+    fc_out = all_layers(feat_norm)
+    z = Dense(num_classes)(fc_out)
+
+    return(z)
+
+def train_fast_rcnn(debug_output=False):
+    # Create the minibatch source
+    minibatch_source = create_reader(map_file)
+
+    # Input variables denoting features, rois and label data
+    image_input = input_variable((num_channels, image_height, image_width))
+    label_input = input_variable((num_classes))
+
+    # define mapping from reader streams to network inputs
+    input_map = {
+        image_input: minibatch_source.streams.features,
+        label_input: minibatch_source.streams.labels}
+
+    # Instantiate the Fast R-CNN prediction model and loss function
+    frcn_output = frcn_predictor(image_input, num_classes)
+    ce = cross_entropy_with_softmax(frcn_output, label_input)
+    pe = classification_error(frcn_output, label_input)
+
+    # Set learning parameters
+    l2_reg_weight = 0.0005
+    lr_per_sample = [0.00001] * 10 + [0.000001] * 5 + [0.0000001]
+    momentum_time_constant = 10
+    lr_schedule = learning_rate_schedule(lr_per_sample, unit=UnitType.sample)
+    mm_schedule = momentum_as_time_constant_schedule(momentum_time_constant)
+
+    # Instantiate the trainer object
+    learner = momentum_sgd(frcn_output.parameters,
+                           lr_schedule,
+                           mm_schedule,
+                           l2_regularization_weight=l2_reg_weight)
+    trainer = Trainer(frcn_output, ce, pe, learner)
+
+    # Get minibatches of images and perform model training
+    print("Training Fast R-CNN model for %s epochs." % max_epochs)
+    log_number_of_parameters(frcn_output)
+    progress_printer = ProgressPrinter(tag='Training', num_epochs=max_epochs)
+    for epoch in range(max_epochs):
+        sample_count = 0
+        while sample_count < epoch_size:
+            data = minibatch_source.next_minibatch(min(mb_size, epoch_size-sample_count),
+                input_map=input_map)
+            trainer.train_minibatch(data)
+            sample_count += trainer.previous_minibatch_sample_count
+            progress_printer.update_with_trainer(trainer, with_metric=True)
+
+        progress_printer.epoch_summary(with_metric=True)
+        frcn_output.save_model(os.path.join(output_model_folder,
+                                            'alexnet_{}.dnn'.format(epoch+1)))
+
+    return
+
+if __name__ == '__main__':
+    train_fast_rcnn()
