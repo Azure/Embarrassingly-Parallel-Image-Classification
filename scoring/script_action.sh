501a9220a80eee618f2f72411fa0d555913e585d
Mary Wahl <mawah@microsoft.com>
2017-03-09 14:38:40 -0500

    Change model file locations to semi-permanent location

--- scoring/script_action.sh
+++ scoring/script_action.sh
@@ -12,11 +12,11 @@ sed -i "s#"\$HOME/anaconda3"#"\$ANACONDA_PREFIX"#g" install-cntk.sh
 ./install-cntk.sh --py-version 35
 
 sudo /usr/bin/anaconda/envs/cntk-py35/bin/pip install pillow
-sudo /usr/bin/anaconda/envs/cntk-py35/bin/pip install tensorflow
+sudo /usr/bin/anaconda/envs/cntk-py35/bin/pip install tensorflow==0.12.1
 
 sudo mkdir /tmp/models
 cd /tmp/models
-wget https://mawahstorage.blob.core.windows.net/models/tf.zip -P /tmp/models
+wget https://mawahstorage.blob.core.windows.net/aerialimageclassification/models/tf.zip -P /tmp/models
 unzip /tmp/models/tf.zip
-wget https://mawahstorage.blob.core.windows.net/models/withcrops_50.dnn -P /tmp/models
+wget https://mawahstorage.blob.core.windows.net/aerialimageclassification/models/withcrops_50.dnn -P /tmp/models
 sudo chmod -R 777 /tmp/models
