cc916a3994efb02f4add5d93215d0ab4ffea0c20
Mary Wahl <mary.e.wahl@outlook.com>
2017-06-07 20:48:54 -0400

    Update scoring scripts to CNTK 2.0 and Tensorflow 1.1

--- scoring/script_action.sh
+++ scoring/script_action.sh
@@ -5,14 +5,14 @@
 
 cntk_home="/usr/hdp/current"
 cd $cntk_home
-curl "https://cntk.ai/BinaryDrop/CNTK-2-0rc1-Linux-64bit-CPU-Only.tar.gz" | tar xzf -
+curl "https://cntk.ai/BinaryDrop/CNTK-2-0-Linux-64bit-CPU-Only.tar.gz" | tar xzf -
 cd ./cntk/Scripts/install/linux 
 sed -i "s#"ANACONDA_PREFIX=\"\$HOME/anaconda3\""#"ANACONDA_PREFIX=\"\/usr/bin/anaconda\""#g" install-cntk.sh
 sed -i "s#"\$HOME/anaconda3"#"\$ANACONDA_PREFIX"#g" install-cntk.sh
 ./install-cntk.sh --py-version 35
 
 sudo /usr/bin/anaconda/envs/cntk-py35/bin/pip install pillow
-sudo /usr/bin/anaconda/envs/cntk-py35/bin/pip install tensorflow==0.12.1
+sudo /usr/bin/anaconda/envs/cntk-py35/bin/pip install tensorflow
 
 sudo mkdir /tmp/models
 cd /tmp/models
